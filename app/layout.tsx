437d970bc0dc1a8d2407662218654a8374980966
goforprodev <goforprodev@gmail.com>
2025-04-09 13:44:39 +0100

    config:added html google tag manager config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -14,25 +15,50 @@ export default function RootLayout({
           content="Flood your inbox with job offers in just 21 days"
         />
         <link rel="icon" href="/favicon.ico" />
-        {/* Hotjar Script */}
-        {/* <Script
-          id="hotjar"
+
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}
-                  h._hjSettings={hjid:5333842,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WBLXD62P');
             `,
           }}
-        /> */}
+        />
+
+        {/* Lightforth Script */}
+        <Script
+          id="lightforth-universal"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var head = document.head;
+              var script = document.createElement('script');
+              script.type = 'text/javascript';
+              script.src = "https://t.lightforth.org/v1/lst/universal-script?ph=b18b330f9b6aca0d4ee43dee583f31d90b7f725528683d35d1beb4819d0cf9c3&tag=!clicked&ref_url=" + encodeURI(document.URL);
+              head.appendChild(script);
+            `,
+          }}
+        />
       </head>
-      <body className={`antialiased`}>{children}</body>
+      <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBLXD62P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
