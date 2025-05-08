fb820fb5188fe495a1a336d0124bc1d918285fc5
Tripleoniks <84743221+Tripleoniks@users.noreply.github.com>
2025-05-08 16:45:04 +0100

    payment page

---
+++ app/confirm-details/page.tsx
@@ -0,0 +1,14 @@
+// import { Suspense } from 'react';
+import VerificationPage from './verifyPage';
+// import Navbar from '@/components/navbar';
+
+const ConfirmPage = () => {
+  return (
+    <div className="">
+      {/* <Navbar onClickScroll={() => console.log('')} /> */}
+      <VerificationPage />
+    </div>
+  );
+};
+
+export default ConfirmPage;
